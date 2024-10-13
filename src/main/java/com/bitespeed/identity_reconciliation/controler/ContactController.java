4081e6159f2751cf3c2264abe379ee41222cd694
Nafees1914 <nafeesahmad1914@gmail.com>
2024-10-14 00:33:31 +0530

    MySql to Postgres instance & Swagger

--- src/main/java/com/bitespeed/identity_reconciliation/controler/ContactController.java
+++ src/main/java/com/bitespeed/identity_reconciliation/controler/ContactController.java
@@ -3,12 +3,17 @@ package com.bitespeed.identity_reconciliation.controler;
 import com.bitespeed.identity_reconciliation.dto.ContactRequestDTO;
 import com.bitespeed.identity_reconciliation.dto.IdentityReconciliationResponseDTO;
 import com.bitespeed.identity_reconciliation.service.ContactService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/identify")
+@Tag(name = "Contact Controller", description = "API to identify or create contact based on email and phone number")
 public class ContactController {
 
     private final ContactService contactService;
@@ -17,6 +22,13 @@ public class ContactController {
         this.contactService = contactService;
     }
 
+    @Operation(summary = "Identify or Create Contact",
+            description = "This endpoint takes an email and/or phone number and identifies or creates a contact.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully identified or created contact"),
+            @ApiResponse(responseCode = "400", description = "Invalid input: Both email and phone number are missing"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
     @PostMapping
     public ResponseEntity<IdentityReconciliationResponseDTO> identifyContact(@RequestBody ContactRequestDTO contactRequestDTO) {
         String email = contactRequestDTO.getEmail();
